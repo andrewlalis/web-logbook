fe21e7ccefe4a4ddb338d4a510c298bea38b3fe1
andrewlalis <andrewlalisofficial@gmail.com>
2023-07-28 15:10:45 -0400

    Upgraded handy-httpd to 7.7.0

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# Build and deploy script for deploying this service to my VPS. Note that
+# you'll need SSH authentication to run this.
+# I just include it here for ease of use.
+
 dub clean
 rm -f web-logbook
-dub build --build=release
+dub build --build=release --compiler=/opt/ldc2/ldc2-1.33.0-linux-x86_64/bin/ldc2
 echo "Stopping web-logbook service."
 ssh -f root@andrewlalis.com 'systemctl stop web-logbook'
 echo "Uploading new binary."
